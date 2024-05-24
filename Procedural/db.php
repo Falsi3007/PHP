5aba4b275e47559bd277dc2fe18f7ab17f72efab
Falsi3007 <falsivadaliya@gmail.com>
2024-05-24 15:08:37 +0530

    add

---
+++ Procedural/db.php
@@ -0,0 +1,31 @@
+
+
+<!-- --------------------MySQLi Procedural---------------------------------------------------- -->
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn="";
+
+
+try{
+$conn = mysqli_connect($servername, 
+                        $username, 
+                        $password,
+                        );
+
+}
+catch(mysqli_sql_exception){
+    echo "could not connected! ";
+}
+
+if($conn){
+    echo "db is connected!";
+}
+
+
+?>
+
+
+
